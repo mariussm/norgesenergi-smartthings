d52ef8b8378accb4d170c67b0ccdfc08dfbcceb6
mariussm <marius.msm@gmail.com>
2017-12-31 11:39:00 +0100

    Initial commit

---
+++ README.ps1
@@ -0,0 +1,5 @@
+# Norgesenergi price smartthings app
+
+Make sure to update with the correct url in the code. The current code is based on my own post code.
+
+Use Chrome and F12 menu while looking up at https://www.norgesenergi.no/produkter/strompriser/, then you'll see the API url. It's an open API with a simple GET :)
